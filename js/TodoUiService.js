82a9e44832878b66cdc874eae5fd650f06b079fa
B0212246 <b0212246@LTB0212246-MAC.local>
2019-07-08 16:19:22 +0530

    completed

--- js/TodoUiService.js
+++ js/TodoUiService.js
@@ -1,14 +1,16 @@
 const TodoUiService = {
     renderTasks: function (targetUL, tasks, updateCallback, deleteCallback, completedCallback) {
+        targetUL.innerHTML='';
         tasks.forEach(task => {
             const editButton = document.createElement('button');
 
-            const doneButton = document.createElement('button');
-
+            const doneCheckbox = document.createElement('input');
+            doneCheckbox.setAttribute("type","checkbox")
+            doneCheckbox.checked=task.isDone;
             const removeButton = document.createElement('button');
 
             editButton.innerHTML = "Edit"
-            doneButton.innerHTML = "Done"
+            //doneCheckbox.innerHTML = "Done"
             removeButton.innerHTML = "Remove"
 
             const inputElement = document.createElement('input');
@@ -16,12 +18,26 @@ const TodoUiService = {
 
             const liElement = document.createElement('li');
 
+            liElement.appendChild(doneCheckbox)
             liElement.appendChild(inputElement)
             liElement.appendChild(editButton)
-            liElement.appendChild(doneButton)
+          
             liElement.appendChild(removeButton)
 
+            editButton.addEventListener('click',function(){
+                updateCallback(task.id,inputElement.value)
+            }.bind(this))
+
+            removeButton.addEventListener('click',function(){
+                deleteCallback(task.id)
+            }.bind(this))
+
+            doneCheckbox.addEventListener('change',function(){
+                completedCallback(task.id,doneCheckbox.checked)
+            }.bind(this))
             targetUL.appendChild(liElement)
+
+
         });
     }
 }
--- js/index.js
+++ js/index.js
@@ -3,20 +3,38 @@ let addTaskButton = null;
 let todoListUl = null;
 
 const todoService = new TodoServices();
+const updateTaskHandler = function (id, text) {
+    //console.log(event)
+    todoService.update(id, text,undefined)
 
+}
+const deleteTaskHandler = function (id) {
+    todoService.remove(id)
+
+}
+const doneTaskHandler = function (id,isDone) {
+    todoService.update(id, undefined, isDone)
+
+}
 const onTodoListDataChange = function () {
-
+    //console.log(todoService)
+    const taskList = todoService.getAll()
+    console.log(taskList)
+    TodoUiService.renderTasks(todoListUl, taskList,
+        updateTaskHandler, deleteTaskHandler, doneTaskHandler)
 }
 
 todoService.addEventListener(onTodoListDataChange)
 
 const addTaskButtonClick = function (event) {
-    console.log('button clicked')
+  //  console.log('button clicked')
     const text = newTaskInput.value;
 
     const newTask = new TaskModel(text, false);
 
     todoService.add(newTask)
+    newTaskInput.value=null;
+   // event.target.value='';
 
     // TodoUiService.renderTasks(todoListUl, [newTask])
 }
@@ -28,6 +46,7 @@ function DOMContentLoadedCallback() {
     todoListUl = document.getElementById('todoListUl');
 
     addTaskButton.addEventListener('click', addTaskButtonClick)
+    onTodoListDataChange();
 }
 
 
